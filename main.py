878eaa663a3aa13d31d33849cdf7cbf1a0939fcd
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-08-14 15:55:03 -1000

    Remove api call and return access token

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import webbrowser
 import requests
-import json
-import os
 from dotenv import dotenv_values, load_dotenv
 
 config = dotenv_values(".env")
@@ -27,12 +25,6 @@ def get_access_token(client_id, client_secret, code, redirect_uri):
     response = requests.post(token_url, data=payload)
     return response.json()  # Contains access_token
 
-def access_canvas_api(access_token):
-    url = "https://byuh.test.instructure.com/api/v1/courses/"
-    headers = {"Authorization": f"Bearer {access_token}"}
-    response = requests.get(url, headers=headers)
-    return response.json()  # Contains the data from Canvas API
-
 client_id = config['CLIENT_ID']
 client_secret = config['CLIENT_SECRET']
 redirect_uri = config['REDIRECT_URI']
@@ -46,8 +38,4 @@ code = get_authorization_code()
 # Step 3: Exchange code for an access token
 token_response = get_access_token(client_id, client_secret, code, redirect_uri)
 access_token = token_response.get('access_token')
-
-# Step 4: Use the access token to access Canvas API
-api_response = access_canvas_api(access_token)
-json_str = json.dumps(api_response, indent=4)
-print(api_response)
+print(f"Access Token: {access_token}")
